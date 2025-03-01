6416f5cebb28e9489e090a308f5e35eba4019a8c
Michael <mjhf89@umsystem.edu>
2025-03-01 16:09:34 -0600

    Endless

--- pickhacks/scripts/game.gd
+++ pickhacks/scripts/game.gd
@@ -1,10 +1,11 @@
 extends Node2D
 
 @onready var tile_map: TileMap = $TileMap
-@onready var character_body: CharacterBody2D = $CharacterBody2D
+@export var speed = 0.8
 
 func _process(delta: float):
-	if character_body.position.x < -270:
+	tile_map.position.x -= speed
+	if tile_map.position.x < -270:
 		tile_map.position.x = 0
 
 #func _change_tile_map():
