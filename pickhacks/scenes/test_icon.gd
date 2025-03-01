658213e2648bd77106c2797f865723b71a86b3a5
Michael <mjhf89@umsystem.edu>
2025-03-01 17:43:09 -0600

    Rudimentary scoring system (NO PISS)

---
+++ pickhacks/scenes/test_icon.gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+@onready var game_manager: Node = %Game_manager
+var speed = 0.8
+func _on_body_entered(body) -> void:
+	game_manager.add_point()# Replace with function body.
+	position.x += 250; 
+
+func _process(delta: float) -> void:
+	position.x -= speed
+	if position.x < -200:
+		position.x = 200
--- pickhacks/scripts/game.gd
+++ pickhacks/scripts/game.gd
@@ -2,11 +2,12 @@ extends Node2D
 
 @onready var tile_map: TileMap = $TileMap
 @export var speed = 0.8
+@onready var tile_map_2: TileMap = $TileMap2
 
 func _process(delta: float):
 	tile_map.position.x -= speed
 	if tile_map.position.x < -270:
 		tile_map.position.x = 0
-
+	
 #func _change_tile_map():
 	#pass 
