1ddcc95e615ccc18bfc71f807a3ab98b76dd0303
Michael <mjhf89@umsystem.edu>
2025-03-01 22:43:57 -0600

    Updated floor

--- pickhacks/control.gd
+++ pickhacks/control.gd
@@ -3,12 +3,11 @@ extends Control
 @onready var start_game: Button = $start_game
 
 func _ready() -> void:
-	print("I have been turned on")
+	pass
 
 func _on_start_game_pressed():
-	print("you turned me on")
 	var game_scene = ResourceLoader.load("res://scenes/game.tscn")
 	if game_scene:
 		get_tree().change_scene_to_packed(game_scene)
 	else: 
-		printerr("I have brought shame upon my bloodline")
+		printerr("Connection Error")
