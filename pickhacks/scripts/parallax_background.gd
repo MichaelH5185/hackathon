41850a257859d3f427e86774dfb8991162e87df6
Carissa Maurer <cgmaurerthr@gmail.com>
2025-03-01 20:46:24 -0600

    added looping background

--- pickhacks/scripts/parallax_background.gd
+++ pickhacks/scripts/parallax_background.gd
@@ -1,7 +1,13 @@
 extends ParallaxBackground
 
+@onready var parallax_layer_2: ParallaxLayer = $ParallaxLayer2
 @onready var parallax_layer: ParallaxLayer = $ParallaxLayer
 @export var sky_speed = 0.1
+@export var background_width = 820
 
 func _process(delta: float) -> void:
-	parallax_layer.motion_offset -= Vector2(sky_speed, 0)
+	#parallax_layer.motion_offset -= Vector2(sky_speed, 0)
+	parallax_layer_2.motion_offset -= Vector2(sky_speed, 0)
+
+	if parallax_layer_2.motion_offset.x <= -background_width:
+		parallax_layer_2.motion_offset.x += background_width
