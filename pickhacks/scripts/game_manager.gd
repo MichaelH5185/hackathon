45830c434a00d77b3e82a0944728c05e274679be
Carissa Maurer <cgmaurerthr@gmail.com>
2025-03-01 22:48:54 -0600

    pulling changes

--- pickhacks/scripts/game_manager.gd
+++ pickhacks/scripts/game_manager.gd
@@ -6,3 +6,6 @@ var score = 0;
 func add_point():
 	score += 1
 	score_label.text = "Score: " + str(score)
+	
+func get_score() -> int:
+	return score
--- pickhacks/scripts/parallax_background.gd
+++ pickhacks/scripts/parallax_background.gd
@@ -1,13 +1,31 @@
 extends ParallaxBackground
 
 @onready var parallax_layer_2: ParallaxLayer = $ParallaxLayer2
-@onready var parallax_layer: ParallaxLayer = $ParallaxLayer
+@onready var game_manager: Node = %Game_manager
+
 @export var sky_speed = 0.1
 @export var background_width = 820
+@export var winning_score = 150
+
+var game_won = false
 
 func _process(delta: float) -> void:
-	#parallax_layer.motion_offset -= Vector2(sky_speed, 0)
+	if game_won:
+		return
+	
 	parallax_layer_2.motion_offset -= Vector2(sky_speed, 0)
 
 	if parallax_layer_2.motion_offset.x <= -background_width:
 		parallax_layer_2.motion_offset.x += background_width
+
+	if game_manager.get_score() >= winning_score:
+		stop_scrolling()
+
+func stop_scrolling():
+	game_won = true
+	parallax_layer_2.visible = false
+	var game_scene = ResourceLoader.load("res://scenes/winner.tscn")
+	if game_scene:
+		get_tree().change_scene_to_packed(game_scene)
+	else:
+		printerr("winner_error")
